bf0d69b8cbfdbb7139c50cda4f30ae1f08de44e4
Yunseok1128 <125713900+Yunseok1128@users.noreply.github.com>
2023-04-10 21:20:44 +0900

    Add files via upload

---
+++ C언어 도전 150제 1~5번/20230410_4/20230410_4/main.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int x = 20, y = 10;
+
+
+	if (x > y)
+	{
+		printf("x의 값은 y의 값보다 크다. \n");
+	}
+	else
+	{
+		printf("x의 값은 y의 값보다 작다. \n");
+	}
+
+	return 0;
+}
